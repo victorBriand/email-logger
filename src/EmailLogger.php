8ee47ed44ef6aa7e397262d10314d40ab4e0110f
Victor Briand <victor.briand78@gmail.com>
2023-09-17 12:42:10 +0100

    EmailSentListener.php created and defined in the ServiceProvider

--- src/EmailLogger.php
+++ src/EmailLogger.php
@@ -4,4 +4,5 @@ namespace Vic\EmailLogger;
 
 class EmailLogger
 {
+    
 }
--- src/EmailLoggerServiceProvider.php
+++ src/EmailLoggerServiceProvider.php
@@ -2,24 +2,29 @@
 
 namespace Vic\EmailLogger;
 
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Support\Facades\Event;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Vic\EmailLogger\Commands\EmailLoggerCommand;
+use Vic\EmailLogger\Listeners\EmailSentListener;
 
 class EmailLoggerServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('email-logger')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_email-logger_table')
             ->hasCommand(EmailLoggerCommand::class);
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        Event::listen(MessageSent::class, EmailSentListener::class);
     }
 }
